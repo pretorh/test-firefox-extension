0e6f0bb6ad3ccd1c7afe2d29924baed632bcd77c
Hendri Pretorius <pretorh@gmail.com>
2018-09-09 13:52:34 +0200

    switch to tab if it already exists

--- README.md
+++ README.md
@@ -7,3 +7,5 @@ create `manifest.json` file
 build `.xpi` file (extention files are plain zip files, with `xpi` extention)
 
 to install, enable unsigned in [firefox settings](about:config) by setting `xpinstall.signatures.required` to false ([reference](https://support.mozilla.org/en-US/kb/add-on-signing-in-firefox?as=u&utm_source=inproduct#w_what-are-my-options-if-i-want-to-use-an-unsigned-add-on-advanced-users))
+
+need `tabs` permission to read tabs
--- background.js
+++ background.js
@@ -5,9 +5,16 @@
 chrome.browserAction.setBadgeText({text: 'test'});
 chrome.browserAction.setBadgeBackgroundColor({color: '#777'});
 
-chrome.browserAction.onClicked.addListener(function(aTab) {
-  chrome.tabs.create({
-    'url': 'https://github.com/pretorh',
-    'active': true
+chrome.browserAction.onClicked.addListener(function(tab) {
+  const URL = 'https://github.com/pretorh';
+  chrome.tabs.query({'url': URL}, (tabs) => {
+    if (tabs.length === 0) {
+      chrome.tabs.create({
+        'url': URL,
+        'active': true
+      });
+    } else {
+      chrome.tabs.update(tabs[0].id, {'active': true});
+    }
   });
 });
